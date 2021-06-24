3368013390cf18e143d8f1b1b5e1e3ea7dcc910b
嘉哥 <867880495@qq.com>
2021-06-25 02:49:16 +0800

    css

--- H5/pages/home/home.vue
+++ H5/pages/home/home.vue
@@ -1,18 +1,20 @@
 <template>
 	<view>
-		<view class="d-flex">
-			<view class="flex-1"></view>
-			<view class="flex-1 d-flex j-center a-center flex-column line-h">
-				<view class="iconf">BTC</view>
-				￥40,959.56亿
-			</view>
-			<view class="flex-1 d-flex j-center a-center flex-column line-h">
-				<view class="iconf">BTC</view>
-				￥40,959.56亿
+		<!-- 行情列表 -->
+		<view class="hqList" style="overflow: hidden;">
+			<block v-for="(item, index) of marketsList" :key="index">
+				<view class="hqInfo">
+					<view class="hqNum" style="float: left; background-color: #d386ff;">{{index + 1}}</view>
+					<view class="hqIcon" style="float: left; margin-left: 20px; background-color: #007AFF;">
+						<image style="width: 36rpx; height: 35rpx;" :src="item.image" >{{item.symbol}}</image>
+						<view class="num">￥{{item.market_cap}}美元</view>
+					</view>
+					<view class="hqAcunt" style="float: left; background-color: #eeeeee;">≈￥{{item.current_price}}</view>
+					<view class="hqPrice" style="float: left; background-color: #eeeeee;">{{item.current_price / 32688}}%</view>
 				</view>
-			<view class="flex: 2.5"></view>
+			</block>
 		</view>
-		<!-- marketsList -->
+		<!-- marketsList 
 		<view v-if="marketsList.length > 0" class="bg-color-white info-list">
 			<view class="info-item" v-for="(item, index) of marketsList" :key="index">
                 <view class="image-content">
@@ -21,11 +23,11 @@
 					<view class="number">￥{{item.market_cap}}美元</view>
 					<view class="number">≈￥{{item.current_price}}</view>
 					<view class="number">{{item.current_price / 32688}}%</view>
-					<!-- 涨跌幅 = 涨跌值 / 昨收盘 * 100% -->
+					涨跌幅 = 涨跌值 / 昨收盘 * 100%
                     <image style="width: 36rpx; height: 35rpx; background-color: #eeeeee;" :src="item.image" >{{item.name}}</image>
                 </view>
 			</view>
-		</view>
+		</view>-->
 	</view>
 </template>
 
@@ -42,7 +44,7 @@
 		onLoad() {
 			console.log('页面加载完成');
 			uni.request({
-			    url: 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false',
+			    url: 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1&page=1&sparkline=false',
 				method: 'get',
 			    success: (res) => {
 			        console.log(res.data);
