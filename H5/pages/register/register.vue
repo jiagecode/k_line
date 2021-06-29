efa95a36e3d80287f0743a4c56e5b9288592b77d
DESKTOP-9FNAP0F\scxfsc <867880495@qq.com>
2021-06-29 10:34:14 +0800

    新增-登录页面

--- H5/pages/register/register.vue
+++ H5/pages/register/register.vue
@@ -1,24 +1,38 @@
 <template>
 	<view class="body">
 		<!-- 密码登录提示 -->
-		<H1 style="margin-top: 80rpx;">密码登录</H1>
+		<H1 style="margin-top: 75rpx;">注册账号</H1>
 		<view style="margin-top: 75rpx;">
 			<uni-forms>
-				<!-- 手机号 -->
+				<!-- 姓名 -->
 				<view class="d-flex a-center" style="height: 80rpx; border-bottom: solid #ececec 2rpx;">
+					<uni-icons type="personadd" color="#b1b1b1" size="20"/>
+					<uni-forms-item label="" name="" style="margin-top: 50rpx;">
+						<uni-easyinput type="text" :inputBorder="false" placeholder="输入姓名" v-model="userName" />
+					</uni-forms-item>
+				</view>
+				<!-- 手机号 -->
+				<view class="d-flex a-center" style="margin-top: 55rpx; height: 80rpx; border-bottom: solid #ececec 2rpx;">
 					<navigator hover-class="none" open-type="navigate" url="../address/address">
 						<text>{{countryAndCode}}</text>
 						<uni-icons type="arrowdown" color="#b1b1b1" style="margin-left: 10rpx;"/>
 					</navigator>
-					<uni-forms-item label="" name="name" style="margin-top: 50rpx;">
+					<uni-forms-item label="" name="" style="margin-top: 50rpx;">
 						<uni-easyinput style="margin-left: 15rpx;" type="number" :inputBorder="false" placeholder="请输入手机号" v-model="phone" />
 					</uni-forms-item>
 				</view>
 				<!-- 密码 -->
 				<view class="d-flex a-center" style="margin-top: 55rpx; height: 80rpx; border-bottom: solid #ececec 2rpx;">
 					<uni-icons type="locked" color="#b1b1b1" size="20"/>
-					<uni-forms-item label="" name="name" style="margin-top: 50rpx;">
-						<uni-easyinput type="password" :inputBorder="false" placeholder="输入密码" v-model="password" />
+					<uni-forms-item label="" name="" style="margin-top: 50rpx;">
+						<uni-easyinput type="password" :inputBorder="false" placeholder="设置登录密码" v-model="passwordOne" />
+					</uni-forms-item>
+				</view>
+				<!-- 确认密码 -->
+				<view class="d-flex a-center" style="margin-top: 55rpx; height: 80rpx; border-bottom: solid #ececec 2rpx;">
+					<uni-icons type="checkbox-filled" color="#b1b1b1" size="20"/>
+					<uni-forms-item label="" name="" style="margin-top: 50rpx;">
+						<uni-easyinput type="password" :inputBorder="false" placeholder="确认登录密码" v-model="passwordTwo" />
 					</uni-forms-item>
 				</view>
 				<!-- 用户协议 -->
@@ -28,9 +42,13 @@
 					<text style="color: #b2b2b2;">我已阅读并同意隐私政策以及用户协议</text>
 				</view>
 				<!-- 登录按钮 -->
-				<view style="margin-top: 75rpx;">
-					<button v-show="isXy" style="background-color: #dddddd; color: #FFFFFF;">登录</button>
-					<button v-show="!isXy" style="background-color: #5586d3; color: #FFFFFF;" @click="toLogin()">登录</button>
+				<view style="margin-top: 65rpx;">
+					<button v-show="isXy" style="background-color: #dddddd; color: #FFFFFF;">注册</button>
+					<button v-show="!isXy" style="background-color: #5586d3; color: #FFFFFF;" @click="toRegister()">注册</button>
+				</view>
+				<!-- 已有账户登录 -->
+				<view class="d-flex a-center j-center" style="margin-top: 35rpx; color: #5586d3;">
+					<text @click="toLoginPage()">已有账户登录</text>
 				</view>
 			</uni-forms>
 		</view>
@@ -43,9 +61,10 @@
 	export default {
 		data() {
 			return {
-				phone: '18066668888',
-				password: '123456',
-				value: 1,
+				phone: '18866668888',
+				passwordOne: '123456',
+				passwordTwo: '123456',
+				userName: 'admin',
 				countryAndCode: '',
 				isXy: true
 			}
@@ -62,11 +81,18 @@
 			this.countryAndCode = country.area_code;
 		},
 		methods: {
-			// 登录
-			toLogin(){
-				
+			// 注册
+			toRegister(){
+				console.log("toLogin");
 				uni.switchTab({
 					url:'../home/home'
+				})
+			},
+			// 到登录页
+			toLoginPage(){
+				console.log("toLogin");
+				uni.redirectTo({
+					url:'../login/login'
 				})
 				
 				// 发送 res.code 到后台换取 openId, sessionKey, unionId
